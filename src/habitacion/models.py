96d148e29825c070cd66c5e8086a61efe938714f
Esteban Bustos <e.bustosf@outlook.com>
2018-06-13 12:54:51 -0400

    habitaciones

--- src/habitacion/models.py
+++ src/habitacion/models.py
@@ -1,5 +1,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from hotel.models import Hotel
 
 # Create your models here.
+class Habitacion(models.Model):
+    TIPOS = (
+        ('1', 'Sola'),
+        ('2', 'Doble')
+    )
+    numero = models.IntegerField()
+    precio = models.IntegerField()
+    capacidad = models.IntegerField()
+    tipo = models.CharField(max_length=1, choices=TIPOS, default=TIPOS[0][0])
+    hotel = models.ForeignKey('hotel.Hotel', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.numero
