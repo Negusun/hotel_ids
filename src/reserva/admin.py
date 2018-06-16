5135ba3dfe7f7702c8a71a55ae066826e0ea804b
Esteban Bustos <e.bustosf@outlook.com>
2018-06-15 22:16:58 -0400

    up

--- src/reserva/admin.py
+++ src/reserva/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Reserva
+
+class AdminReserva(admin.ModelAdmin):
+    list_display = ['id', 'fecha_ingreso', 'fecha_salida', 'valor_reserva', 'estado_reserva', 'habitacion']
+    #list_filter = ['categoria']
+    #list_editable = ['nombre', 'ubicacion', 'categoria']
+    #search_fields = ['nombre']
+    class Meta:
+        model = Reserva
+
+admin.site.register(Reserva, AdminReserva)
--- src/reserva/models.py
+++ src/reserva/models.py
@@ -3,3 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Reserva(models.Model):
+    ESTADOS = (
+        ('1', 'Disponible'),
+        ('2', 'Reservada')
+    )
+    fecha_ingreso = models.DateTimeField(auto_now_add=False, auto_now=False)
+    fecha_salida = models.DateTimeField(auto_now_add=False, auto_now=False)
+    valor_reserva = models.IntegerField()
+    estado_reserva = models.CharField(max_length=1, choices=ESTADOS, default=ESTADOS[0][0])
+    habitacion = models.ForeignKey('habitacion.Habitacion', on_delete=models.CASCADE, null=True)
+
+    def __unicode__(self):
+        return str(self.id)
