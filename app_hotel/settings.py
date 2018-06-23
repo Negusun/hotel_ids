b4015a2d0cd91ed8727c6bbafaa92e8ee1b72645
Esteban Bustos <e.bustosf@outlook.com>
2018-06-23 00:57:05 -0400

    configuracion de archivos estaticos en dev + primera template

--- app_hotel/settings.py
+++ app_hotel/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'app_hotel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#archivos estaticos css js imagenes
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    #'/var/www/static/',
+]
