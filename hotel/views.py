1a72f69df7750fe4195d09150bbba9a0c1815b24
Esteban Bustos <e.bustosf@outlook.com>
2018-06-24 04:24:17 -0400

    parametro de busqueda ciudad

--- hotel/views.py
+++ hotel/views.py
@@ -7,8 +7,9 @@ import json
 
 # Create your views here.
 def buscar(request):
-    precio_min = request.GET.get('precio_min') #diccionario
-    precio_max = request.GET.get('precio_max') #diccionario
+    precio_min = request.GET.get('precio_min')
+    precio_max = request.GET.get('precio_max')
+    ciudad = request.GET.get('ciudad')
 
     if not precio_min:
         precio_min = 10000
@@ -16,13 +17,21 @@ def buscar(request):
     if not precio_max:
         precio_max = 100000
 
-    habitaciones = Habitacion.objects.filter(precio__range=(precio_min, precio_max)).order_by('precio')
+    if ciudad:
+        hoteles_id = Hotel.objects.filter(ciudad=ciudad)
+        habitaciones = Habitacion.objects.filter(hotel__in=hoteles_id).filter(precio__range=(precio_min, precio_max)).order_by('precio')
+    if not ciudad:
+        habitaciones = Habitacion.objects.filter(precio__range=(precio_min, precio_max)).order_by('precio')
+
+    ciudades = Ciudad.objects.all()
+
 
     context = {
         'title' : 'Habitaciones',
         'habitaciones' : habitaciones,
         'precio_min': precio_min,
-        'precio_max': precio_max
+        'precio_max': precio_max,
+        'ciudades': ciudades
     }
     return render(request, 'habitacion/listar.html', context)
 
