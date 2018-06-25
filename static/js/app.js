b12c583095ec0c1177e3dede1f90854006357a2e
Esteban Bustos <e.bustosf@outlook.com>
2018-06-25 03:38:30 -0400

    agregado libreria js datepicker range para input de fechas

---
+++ static/js/app.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+  // Configuracion del datepicker
+
+  $(function() {
+    $('input[name="fechas"]').daterangepicker({
+      opens: 'right',
+      "buttonClasses": "btn",
+      "applyButtonClasses": "btn-info",
+      "cancelClass": "btn-danger",
+      "locale": {
+        "format": "MM/DD/YYYY",
+        "separator": " - ",
+        "applyLabel": "Aceptar",
+        "cancelLabel": "Cancelar",
+        "fromLabel": "Desde",
+        "toLabel": "A",
+        "customRangeLabel": "Custom",
+        "daysOfWeek": [
+            "Dom",
+            "Lun",
+            "Mar",
+            "Mie",
+            "Jue",
+            "Vie",
+            "Sab"
+        ],
+        "monthNames": [
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+            "Julio",
+            "Agusto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre"
+        ],
+        "firstDay": 1
+      }
+    }, function(start, end, label) {
+      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+  });
+});
